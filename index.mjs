0a7b4c14b00afa8f81a73bae4d4018de625f27ac
axwhyzee <siah_weehung@yahoo.com>
2023-05-25 12:39:41 +0800

    integrate webhook

--- index.mjs
+++ index.mjs
@@ -1,13 +1,13 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import fetch from 'node-fetch';
 
 import { prerender } from './cola_prerender.mjs';
 import { initialise, find_all, insert_many } from './database_handler.mjs';
 
-
 const app = express();
-const PORT = 10000; 
+const PORT = 10001; 
 
 // for serverless (Cyclic), connections should be established before listening
 let database_client = await initialise();
@@ -15,23 +15,25 @@ let database_client = await initialise();
 
 app.get('/', async (req, res) => {
     console.log('GET request');
-    const prev_database_name = req.query.prev;
-    const new_database_name = req.query.new;
+    const raw_database_name = req.query.dbraw;
+    const rendered_database_name = req.query.dbrendered;
     const webhook = req.query.webhook;
 
-    console.log(prev_database_name, new_database_name);
+    console.log('RAW DB:', raw_database_name)
+    console.log('RENDERED DB:', rendered_database_name)
+    console.log('WEBHOOK:', webhook);
 
-    const edges = await find_all(database_client, prev_database_name);
+    const edges = await find_all(database_client, raw_database_name);
     const coordinates = await prerender(edges);
 
-    insert_many(database_client, new_database_name, coordinates);
+    insert_many(database_client, rendered_database_name, coordinates);
 
     // send to webhook to coordinate the change to new set of data
-    await fetch(webhook);
+    fetch(webhook);
 
     res.status(200).send('success');
 });
 
 app.listen(PORT, ()=>{
-    console.log('listening on port')
+    console.log('listening on port ' + PORT)
 });
